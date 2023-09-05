87b9daba402be8581e6955be6109a43d62c2eda6
carllopessil <carlosandrade60@hotmail.com>
2023-09-05 14:52:08 -0300

    Ajuste Script do banco.
    Inclusão de metodos no Script.SQL
    metodo que valida se a tabela produtos existe e descarta ela!
    metodo que cria a tabela

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -15,3 +15,18 @@ CREATE TABLE UsuarioBackOffice (
 -- Inserir um usuário Admin
 INSERT INTO UsuarioBackOffice (Nome, Email, Senha, Grupo, Status, CPF)
 VALUES ('admin', 'admin@admin', '$2a$10$JdPVJ.lNR.uwv4HPlJeAMuA8h1ZFve93cddOf8coYrnNGjKX/Ujsi', 'Admin Group', TRUE, '123.456.789-00');
+
+-- Verificar se a tabela existe e descartá-la se já existir
+DROP TABLE IF EXISTS Produtos;
+
+-- Criação da tabela de produtos
+CREATE TABLE Produtos (
+    ProdutoID INT PRIMARY KEY AUTO_INCREMENT,
+    NomeProduto VARCHAR(200) NOT NULL,
+    StatusProduto boolean NOT NULL,
+    Avaliacao DECIMAL(2,1) CHECK (Avaliacao >= 1 AND Avaliacao <= 5),
+    DescricaoDetalhada TEXT,
+    PrecoProduto DECIMAL(10,2),
+    QtdEstoque INT,
+	ImagePATH VARCHAR(200)
+);
